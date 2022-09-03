0f4376765b3a9687469830bbc22d2f8a2054ed33
Alexey8288 <alex.lux7777777@gmail.com>
2022-09-03 22:16:12 +0300

    hamburger

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,9 @@ $(document).ready(function () {
 	// !заменили ID на наш клас .header-navbar__btn
 	$('.header-navbar__btn').click(function(){
 		$(this).toggleClass('open');
-		$('.header-navbar__list').toggleClass('header-navbar__list--active');
+		// способ 1
+		// $('.header-navbar__list').toggleClass('header-navbar__list--active');
+		// способ 2
+		$('.header-navbar__list').stop(true,true).slideToggle(500);
 	});
 });
