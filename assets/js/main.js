461b721ee5df0967a826a4b96603a6a17d2bdf3f
Alexey8288 <alex.lux7777777@gmail.com>
2022-09-03 18:59:50 +0300

    8

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,6 @@ $(document).ready(function () {
 	// !заменили ID на наш клас .header-navbar__btn
 	$('.header-navbar__btn').click(function(){
 		$(this).toggleClass('open');
+		$('.header-navbar__list').toggleClass('open');
 	});
 });
