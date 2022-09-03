fb31b4effff39008cdc2cbeca3f7846710cc645c
Alexey8288 <alex.lux7777777@gmail.com>
2022-09-03 15:56:39 +0300

    8

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// !для информации значек $ это начит вызов библиотеки jquery
+
+$(document).ready(function () {
+	// !заменили ID на наш клас .header-navbar__btn
+	$('.header-navbar__btn').click(function(){
+		$(this).toggleClass('open');
+	});
+});
